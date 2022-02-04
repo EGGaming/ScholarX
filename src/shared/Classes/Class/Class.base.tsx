ba60b274b6bb1888abd1a45190fcfcae321eed34
specify_ <dieptuber@gmail.com>
2022-02-03 22:44:18 -0700

    Added a container to fix text overflow

--- src/shared/Classes/Class/Class.base.tsx
+++ src/shared/Classes/Class/Class.base.tsx
@@ -2,7 +2,6 @@ import styled, { css } from 'styled-components/native';
 
 export const GradeSymbolContainer = styled.View`
   ${(props) => css`
-    background-color: ${props.theme.palette.background.default};
     padding: ${props.theme.spacing(1)};
     width: 100px;
     height: 100px;
@@ -10,5 +9,12 @@ export const GradeSymbolContainer = styled.View`
     align-items: center;
     display: flex;
     border-radius: ${props.theme.borderRadius}px;
+  `}
+`;
+
+export const ClassInfoContainer = styled.View`
+  ${(props) => css`
+    margin: ${props.theme.spacing(0, 3, 0, 0)};
+    flex-shrink: 1;
   `}
 `;
