fffd57663e635c0a60c18cc32b12333b72fcda48
specify_ <dieptuber@gmail.com>
2022-02-04 00:52:19 -0700

    View grades for different semester

--- src/shared/Classes/Classes.tsx
+++ src/shared/Classes/Classes.tsx
@@ -4,12 +4,14 @@ import Chip from '@components/Chip/Chip';
 import Flex from '@components/Flex/Flex';
 import Icon from '@components/Icon/Icon';
 import IconButton from '@components/IconButton/IconButton';
+import Loader from '@components/Loader/Loader';
 import Skeleton from '@components/Skeleton/Skeleton';
 import Space from '@components/Space/Space';
 import Typography from '@components/Typography/Typography';
 import { useClassSchedule } from '@context/ClassScheduleContext/ClassScheduleContext';
 import { useGradebook } from '@context/GradebookContext/GradebookContext';
 import { useStudentVue } from '@context/StudentVueClientContext/StudentVueClientContext';
+import ClassSkeleton from '@shared/Classes/Class/ClassSkeleton';
 import { ClassesHeaderContainer } from '@shared/Classes/Classes.base';
 import { EventsContainer } from '@shared/Events/Events.base';
 import { useAppTheme } from '@theme/core';
@@ -17,51 +19,53 @@ import useStateInitializer from '@utilities/useStateInitializer';
 import { getQuarter } from 'date-fns';
 import React from 'react';
 import { ScrollView } from 'react-native';
+import { HoldItem } from 'react-native-hold-menu';
+import { MenuItemProps } from 'react-native-hold-menu/lib/typescript/components/menu/types';
 import Class from './Class/Class';
-
-const currentSemester = () => {
-  switch (getQuarter(Date.now())) {
-    case 1:
-    default:
-    case 2:
-      return 1;
-    case 3:
-    case 4:
-      return 2;
-  }
-};
 
 const Classes: React.FC = () => {
   const [client] = useStudentVue();
   const [schedule, setSchedule] = useClassSchedule();
   const [gradebook, setGradebook] = useGradebook();
-  useStateInitializer(() => client.classSchedule(currentSemester()), setSchedule);
+  useStateInitializer(() => client.classSchedule(), setSchedule);
   useStateInitializer(() => client.gradebook(), setGradebook);
-
+  const menuItems = React.useMemo(
+    () =>
+      gradebook && [
+        { isTitle: true, text: 'Select Term' },
+        ...gradebook.periods.map((period) => ({
+          text: period.name,
+          onPress: async () => {
+            setGradebook(undefined);
+            const gradebookAtPeriod = await client.gradebook(period.index);
+            setGradebook(gradebookAtPeriod);
+          },
+        })),
+      ],
+    [gradebook, client, setGradebook]
+  );
   return (
     <Space spacing={1} direction='vertical'>
       <ClassesHeaderContainer>
         <Typography bold variant='h3'>
           My Classes
         </Typography>
-        <Button
-          onPress={() => {}}
-          title='S2'
-          size='small'
-          icon={<Icon bundle='Feather' name='calendar' />}
-          iconPlacement='right'
-        />
+        <HoldItem items={menuItems ? menuItems : []} activateOn='tap'>
+          <Button
+            title={gradebook ? gradebook.currentPeriod.name : 'Loading...'}
+            size='small'
+            icon={gradebook ? <Icon bundle='Feather' name='calendar' /> : <Loader />}
+            disabled={!gradebook}
+            variant='outlined'
+          />
+        </HoldItem>
       </ClassesHeaderContainer>
       <Flex direction='column'>
         {schedule && gradebook
           ? schedule.classes.map((classSchedule, i) => (
               <Class key={classSchedule.name} class={classSchedule} classInfo={gradebook.classes[i]} />
             ))
-          : new Array(6).fill('').map((_, i) => (
-              <Card key={i}>
-                <Skeleton.Typography variant='h3' width={300} />
-              </Card>
-            ))}
+          : new Array(6).fill('').map((_, i) => <ClassSkeleton key={i} />)}
       </Flex>
     </Space>
   );
